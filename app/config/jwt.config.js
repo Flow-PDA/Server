4ba67d8daa80ce3730b79e8beab0b94ceef3299e
bkkmw <roboticts49@gmail.com>
2024-03-17 13:36:42 +0900

    fix: add default jwt key

--- app/config/jwt.config.js
+++ app/config/jwt.config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  secretKey: process.env.JWT_SECRET,
+  secretKey: process.env.JWT_SECRET || "JWT_SECRET_KEY",
   options: {
     algorithm: "HS256",
     expiresIn: 60 * 60 * 24,
